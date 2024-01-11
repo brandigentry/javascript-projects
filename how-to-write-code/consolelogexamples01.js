ff75728980f190fdd52ba3c7ff9f4472845136ed
Brandi Gentry <brandigentry@Brandis-MacBook-Air.local>
2024-01-11 14:34:44 -0600

    Declared and initialized variables for exercise 1

--- how-to-write-code/consolelogexamples01.js
+++ how-to-write-code/consolelogexamples01.js
@@ -4,3 +4,15 @@ console.log("What","do","commas","do?");
 console.log("Does", "adding",      "space", "matter?");
 console.log('Launch' + 'Code');
 console.log("LaunchCode was founded in", 2013);
+
+let shuttleName = ("Determination");
+let shuttleSpeedMph = (17500);
+let distanceToMarsKm = (225000000);
+let distanceToMoonKm = (384400);
+const milesPerKm = (0.621);
+
+console.log(typeof shuttleName);
+console.log(typeof shuttleSpeedMph);
+console.log(typeof distanceToMarsKm);
+console.log(typeof distanceToMoonKm);
+console.log(typeof milesPerKm);
