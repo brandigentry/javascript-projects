b2b2e5ab277f0863c8316ce960594964f363a01c
Brandi Gentry <brandigentry@Brandis-MacBook-Air.local>
2024-02-05 13:16:18 -0600

    Completed Modules exercises.

--- modules/exercises/index.js
+++ modules/exercises/index.js
@@ -1,8 +1,9 @@
 //Import modules:
-const input = //Import readline-sync.
-const averages = //Import functions from averages.js.
-const printAll = //Import function from display.js.
-const randomSelect = //Import function from randomSelect.js.
+const input = require('readline-sync'); //Import readline-sync.
+const averages = require('./ScoreCalcs/averages'); //Import functions from averages.js.
+const printAll = require('./display.js'); //Import function from display.js.
+const randomSelect = require('./randomSelect.js'); //Import function from randomSelect.js.
+const { averageForTest, averageForStudent } = require('./ScoreCalcs/averages');
 
 //Candidate data:
 let astronauts = ['Fox','Turtle','Cat','Hippo','Dog'];
@@ -18,22 +19,23 @@ for (let i = 0; i<prompts.length; i++){
   let response = input.question(`Would you like to ${prompts[i]}? Y/N: `);
   if (response.toLowerCase()==='y'){
     if (i===0){
-      //Call 'printAll' here and pass in all necessary arguments.
+      console.log(printAll(astronauts, testTitles, scores));//Call 'printAll' here and pass in all necessary arguments.
     } else if (i===1){
       for (let j = 0; j<testTitles.length; j++){
-        let avg = //Call 'averageForTest' here. Pass in j and scores as arguments.
+        let avg = console.log(averageForTest(j, scores)); //Call 'averageForTest' here. Pass in j and scores as arguments.
         console.log(`${testTitles[j]} test average = ${avg}%.`);
       }
     } else if (i===2){
       for (let j = 0; j<astronauts.length; j++){
-        let avg = //Call 'averageForStudent' here. Pass in j and scores as arguments.
+        let avg = console.log(averages.averageForStudent(j, scores)); //Call 'averageForStudent' here. Pass in j and scores as arguments.
         console.log(`${astronauts[j]}'s test average = ${avg}%.`);
       }
     } else {
-      let walker = //Call 'randomSelect' to pick a spacewalker from the astronauts array.
+      let walker = console.log(randomSelect(astronauts)) //Call 'randomSelect' to pick a spacewalker from the astronauts array.
       console.log(`${walker} is the next spacewalker.`);
     }
   } else {
     console.log("Option skipped.");
   }
 }
+
